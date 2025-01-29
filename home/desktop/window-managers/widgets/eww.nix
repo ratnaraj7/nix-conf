28c8268d6e88886425abe1f227f86fa09a867894
foo@example.com <foo@example.com>
2025-01-30 01:27:37 +0530

    fix(eww): fix config path

--- home/desktop/window-managers/widgets/eww.nix
+++ home/desktop/window-managers/widgets/eww.nix
@@ -1,6 +1,6 @@
 {pkgs, user,...}: {
-  xdg.configFile."eww.yuck".text = user.rice.ewwYuck;
-  xdg.configFile."eww.css".text = user.rice.ewwCss;
+  xdg.configFile."eww/eww.yuck".text = user.rice.ewwYuck;
+  xdg.configFile."eww/eww.css".text = user.rice.ewwCss;
   home.packages = [
     pkgs.eww
   ];
