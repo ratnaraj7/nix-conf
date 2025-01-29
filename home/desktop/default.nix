f9e08496667b0807f50e7c8ca5867e69123a0058
foo@example.com <foo@example.com>
2025-01-30 01:45:29 +0530

    chore: remove unnecessary default.nix

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,6 +1,34 @@
 {
+  pkgs,
+  user,
+  lib,
+  swww,
+  ...
+}:
+let
+  hyprlandOptions = lib.head (lib.filter (wm: wm.name == "Hyprland") user.wms);
+  #i3Options = lib.head(lib.filter (wm: wm.name == "i3") user.wms);
+
+  wallpaperFile = pkgs.fetchurl {
+    url = user.rice.wallpaper or "https://images.pexels.com/photos/1876876/pexels-photo-1876876.jpeg";
+    sha256 = user.rice.wallpaperSha256 or "1ljgq3n01kfcffic2wal476r07ga6b3s9hljggwnndgbsx7spd7b";
+  };
+in
+{
+  home.packages = [
+    swww.packages.${pkgs.system}.swww
+  ];
+  xdg.configFile."wallpaper".source = wallpaperFile;
   imports = [
-    ./desktop-environments
-    ./window-managers
+    (
+      if hyprlandOptions != null then
+        import ./window-managers/hyprland.nix {
+          inherit hyprlandOptions;
+          extraConfig = user.rice.hyprland;
+        }
+      else
+        null
+    )
+    #(if i3Options != null then import ./i3.nix { inherit i3Options; } else null)
   ];
 }
