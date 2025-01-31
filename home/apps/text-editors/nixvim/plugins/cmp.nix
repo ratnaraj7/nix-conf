9c5fe23f019d38316799d5aabfb5f97ea47a64c3
foo@example.com <foo@example.com>
2025-01-31 21:37:49 +0530

    feat(nixvim): add lsp servers

---
+++ home/apps/text-editors/nixvim/plugins/cmp.nix
@@ -0,0 +1,71 @@
+{ pkgs, ... }:
+{
+  programs.nixvim = {
+    plugins = {
+      cmp = {
+        enable = true;
+        autoEnableSources = false;
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+          snippet.expand = ''
+            function(args)
+              luasnip.lsp_expand(args.body)
+            end
+          '';
+          completion.completeopt = "menu,menuone,noinsert";
+          mapping = {
+            "<C-n>" = "cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })";
+            "<C-p>" = "cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-j>" = "cmp.mapping.scroll_docs(4)";
+            "<C-k>" = "cmp.mapping.scroll_docs(-4)";
+          };
+          experimental = {
+            ghost_text = {
+              hl_group = "LspCodeLens";
+            };
+          };
+          formatting.format = ''
+            require("tailwindcss-colorizer-cmp").formatter
+          '';
+        };
+      };
+      cmp-nvim-lsp.enable = true;
+      cmp-path.enable = true;
+      cmp-buffer.enable = true;
+      luasnip = {
+        enable = true;
+        filetypeExtend = {
+          javascript = [ "html" ];
+          javascriptreact = [ "html" ];
+          typescriptreact = [ "html" ];
+          htmldjango = [ "html" ];
+          templ = [ "html" ];
+        };
+        fromVscode = [ { } ];
+      };
+    };
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "tailwindcss-colorizer-cmp";
+        src = pkgs.fetchFromGitHub {
+          owner = "roobert";
+          repo = "tailwindcss-colorizer-cmp.nvim";
+          rev = "3d3cd95e4a4135c250faf83dd5ed61b8e5502b86";
+          hash = "sha256-PIkfJzLt001TojAnE/rdRhgVEwSvCvUJm/vNPLSWjpY=";
+        };
+      })
+    ];
+    extraConfigLua = ''
+      require("tailwindcss-colorizer-cmp").setup({
+      	color_square_width = 2,
+      })
+    '';
+  };
+}
--- home/apps/text-editors/nixvim/plugins/lsp.nix
+++ home/apps/text-editors/nixvim/plugins/lsp.nix
@@ -2,7 +2,94 @@
   programs.nixvim.plugins.lsp = {
     enable = true;
     servers = {
+      nixd.enable = true;
       ts_ls.enable = true;
+      templ.enable = true;
+      astro.enable = true;
+      denols = {
+        enable = true;
+        rootDir = "require(\"lspconfig\").util.root_pattern(\"deno.json\", \"deno.jsonc\")";
+      };
+      eslint.enable = true;
+      html = {
+        enable = true;
+        filetypes = [
+          "html"
+          "templ"
+          "htmldjango"
+        ];
+      };
+      htmx = {
+        enable = true;
+        filetypes = [
+          "html"
+          "templ"
+          "htmldjango"
+        ];
+      };
+      tailwindcss = {
+        enable = true;
+        filetypes = [
+          "html"
+          "templ"
+          "htmldjango"
+          "jsx"
+          "typescriptreact"
+          "rust"
+          "templ"
+        ];
+      };
+      lua_ls.enable = true;
+      clangd.enable = true;
+      pylsp.enable = true;
+      docker_compose_language_service.enable = true;
+      dockerls.enable = true;
+      terraformls = {
+        enable = true;
+        filetypes = [
+          "terraform"
+          "tf"
+        ];
+      };
+      gopls = {
+        enable = true;
+        cmd = [ "gopls" ];
+        filetypes = [
+          "go"
+          "gomod"
+          "gowork"
+          "gotmpl"
+        ];
+        rootDir = "require(\"lspconfig\").util.root_pattern(\"go.work\", \"go.mod\", \".git\")";
+        settings = {
+          gopls = {
+            gofumpt = true;
+          };
+        };
+      };
+      #rust_analyzer = {
+      #  enable = true;
+      #  settings = {
+      #    "rust-analyzer" = {
+      #      imports = {
+      #        granularity = {
+      #          group = "module";
+      #        };
+      #        prefix = "self";
+      #      };
+      #      cargo = {
+      #        buildScripts.enable = true;
+      #      };
+      #      procMacro.enable = true;
+      #      checkOnSave = {
+      #        command = "clippy";
+      #      };
+      #      completion = {
+      #        autoimport.enable = true;
+      #      };
+      #    };
+      #  };
+      #};
     };
   };
 }
