1aa415daec4a3948091af682b85e930ee1f3d6d4
foo@example.com <foo@example.com>
2025-02-01 23:46:04 +0530

    ✅(nixvim): import all plugins

--- home/apps/text-editors/nixvim/plugins/default.nix
+++ home/apps/text-editors/nixvim/plugins/default.nix
@@ -1,8 +1,18 @@
 {
   imports = [
-    ./lsp.nix
     ./cmp.nix
     ./colorizer.nix
-    ./codeium.nix
+    ./conform.nix
+    ./fidget.nix
+    ./flit.nix
+    ./hardline.nix
+    ./lsp.nix
+    ./mini.nix
+    ./nvim-lint.nix
+    ./nvim-ts-autotag.nix
+    ./oil.nix
+    ./telescope.nix
+    ./treesitter-textobjects.nix
+    ./treesitter.nix
   ];
 }
