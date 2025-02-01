363250bb1429eb18e913f02675548622a79ea7be
foo@example.com <foo@example.com>
2025-02-01 23:43:58 +0530

    ✅(nixvim): add fidget

---
+++ home/apps/text-editors/nixvim/plugins/fidget.nix
@@ -0,0 +1,12 @@
+{
+  programs.nixvim.plugins.fidget = {
+    enable = true;
+    settings = {
+      notification = {
+        window = {
+          align = "top";
+        };
+      };
+    };
+  };
+}
