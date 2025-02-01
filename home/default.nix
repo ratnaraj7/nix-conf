913aa0251216d3003b78cc28ad7d47ac164c6433
foo@example.com <foo@example.com>
2025-02-02 01:17:56 +0530

    ✅(home): add clipboards

--- home/default.nix
+++ home/default.nix
@@ -11,7 +11,8 @@
     ./display-managers
     ./desktop
     ./apps
-    ./wallpaper.nix {}
+    ./wallpaper.nix
+    { }
   ];
   home.username = user.username;
   home.homeDirectory = "/home/" + user.username;
@@ -19,34 +20,39 @@
     "Xcursor.size" = 16;
     "Xft.dpi" = 172;
   };
-  home.packages = with pkgs; [
-    # archives
-    zip
-    xz
-    unzip
-    p7zip
+  home.packages =
+    with pkgs;
+    [
+      # archives
+      zip
+      xz
+      unzip
+      p7zip
 
-    # utils
-    plocate
-    ripgrep
-    jq
-    eza
-    fzf
+      # utils
+      plocate
+      ripgrep
+      jq
+      eza
+      fzf
 
-    # misc
-    file
-    which
-    tree
-    nautilus
+      # misc
+      file
+      which
+      tree
+      nautilus
+      wl-clipboard
+      xclip
 
-    # remove these fonts, and use
-    # fonts according to themes
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    nerd-fonts.fira-code
-    nerd-fonts.symbols-only
-  ] ++ (map (fontName: pkgs."${fontName}") user.rice.fonts);
+      # remove these fonts, and use
+      # fonts according to themes
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      nerd-fonts.fira-code
+      nerd-fonts.symbols-only
+    ]
+    ++ (map (fontName: pkgs."${fontName}") user.rice.fonts);
   fonts.fontconfig.enable = true;
   programs.git = {
     enable = true;
